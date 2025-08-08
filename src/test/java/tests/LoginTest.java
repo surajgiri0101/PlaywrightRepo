796274cd1d7331f6bf688467058c23fe7527ff4c
Suraj Giri <suraj.giri@dreamorbit.in>
2025-08-08 16:33:39 +0530

    Initial commit fourth

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,12 +1,16 @@
 package tests;
 
 import base.BaseTest;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import utils.ConfigReader;
 
 public class LoginTest extends BaseTest {
 
+	@BeforeMethod
     @Test
     public void loginTest() {
         String username = ConfigReader.getProperty("username");
@@ -14,5 +18,13 @@ public class LoginTest extends BaseTest {
 
         LoginPage loginPage = new LoginPage(page);
         loginPage.login(username, password);
+    }
+    
+    
+    @AfterMethod
+    public void tearDown() {
+        if (playwright != null) {
+            playwright.close();
+        }
     }
 }
