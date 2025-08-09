5a29a8f836b8e9ee06797820d77bf8a0f9ccd924
Suraj Giri <suraj.giri@dreamorbit.in>
2025-08-09 14:48:17 +0530

    Initial commit thirteenth

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -12,7 +12,7 @@ public class BaseTest {
     @BeforeMethod
     public void setup() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         context = browser.newContext();
         page = context.newPage();
     }
