0662bf6313967f2ca8a0346f6152ee0c9bffbdb2
Suraj Giri <suraj.giri@dreamorbit.in>
2025-08-09 16:19:53 +0530

    Initial commit seventeenth

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,6 +1,10 @@
 package base;
 
 import com.microsoft.playwright.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
 import org.testng.annotations.*;
 import utils.ReportManager;
 import com.aventstack.extentreports.ExtentTest;
